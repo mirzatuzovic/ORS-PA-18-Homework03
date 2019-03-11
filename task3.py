20970b6a8edbd2a785fdbefc4c26bc7ea19bb7c2
Mirza Tuzovic <Mirza.Tuzovic@udg.edu.me>
2019-03-11 22:52:42 +0100

    task 3

--- task3.py
+++ task3.py
@@ -11,3 +11,29 @@
 * Use main() function to test your solution.
 ===================================================
 """
+
+def gcd(a,b):
+
+    r = a % b
+
+    while(r != 0):
+
+        a = b
+
+        b = r
+
+        q = int(a/b)
+
+        r = a - (b * q)
+
+    return b
+
+def main():
+
+    a = abs(int(input("First number: ")))
+
+    b = abs(int(input("Second number: ")))
+
+    print(gcd(a,b))
+
+main()
